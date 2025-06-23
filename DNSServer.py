cabdda871f98d73e60003484152a3e51e155d1e4
Trenton Tan <tmt381@nyu.edu>
2025-06-23 19:48:04 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -128,8 +128,8 @@ def run_dns_server():
                     for pref, server in answer_data:
                         rdata_list.append(MX(dns.rdataclass.IN, dns.rdatatype.MX, pref, server))
                 elif qtype == dns.rdatatype.SOA:
-                    mName, rName, serial, refresh, retry, expire, timeToLive = answer_data # What is the record format? See dns_records dictionary. Assume we handle @, Class, TTL elsewhere. Do some research on SOA Records
-                    rdata = SOA(dns.rdataclass.IN, dns.rdatatype.SOA, mName, rName, serial, refresh, retry, expire, timeToLive) # follow format from previous line
+                    mName, rName, serial, refresh, retry, expire, minimum = answer_data # What is the record format? See dns_records dictionary. Assume we handle @, Class, TTL elsewhere. Do some research on SOA Records
+                    rdata = SOA(dns.rdataclass.IN, dns.rdatatype.SOA, mName, rName, serial, refresh, retry, expire, minimum) # follow format from previous line
                     rdata_list.append(rdata)
                 else:
                     if isinstance(answer_data, str):
